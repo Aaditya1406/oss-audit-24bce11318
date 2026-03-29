6d175f4b532a316da31b4f4511eb6779e7f3ed57
Aaditya Chauhan <aaditya.24bce11318@vitbhopal.ac.in>
2026-03-29 23:17:02 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Script 1: System Identity Report
-# Author: Vishal Disale [cite: 103]
+# Author: Aaditya Chauhan
 
-STUDENT_NAME="Vishal Disale"
+STUDENT_NAME="Aaditya Chauhan"
 SOFTWARE_CHOICE="Git"
 KERNEL=$(uname -r)
 DISTRO=$(cat /etc/os-release | grep "PRETTY_NAME" | cut -d'"' -f2)
